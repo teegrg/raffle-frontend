4061e27be3a1cd828daa36dfe34d5527e75a2eed
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-04-09 10:35:58 -0400

    fetched data from api and added router routes to home and registar page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Registar from "./pages/Registar";
+
+const API = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [raffles, setRaffles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError("");
+      const response = await fetch(`${API}/api/raffles`);
+      const { data, error: errMessage } = await response.json();
+      if (response.ok) {
+        setRaffles(data);
+      } else {
+        throw new Error(errMessage);
+      }
+    } catch (err) {
+      setError(err.Message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  
+  useEffect(() => {
+    fetchData();
+  },
+  []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {loading ? "loading..." : <Route path="/" element={<Home raffles={raffles}/>} />}
+          <Route path="/registar/:id" element={<Registar raffles={raffles}/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
