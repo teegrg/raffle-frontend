b2a7f8afda44641a2436e2be2cf2d8cccc0dd184
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-04-09 10:38:13 -0400

    added hoem and registar pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import AllRaffles from "../components/AllRaffles";
+import "../style/Home.css";
+
+
+const API = process.env.REACT_APP_API_URL;
+
+function Home({ raffles }) {
+  const [key, setKey] = useState({
+    name: "",
+    key: ""
+});
+
+  const handleChange = (e) => {
+    setKey((prevVal) => {
+      return  {...prevVal, [e.target.name]: e.target.value}
+    })
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, key: secretToken } = key; // Destructure name and key from key object
+    try {
+      const response = await fetch(`${API}/api/raffles`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, key: secretToken }), // Use name and secretToken here
+      });
+      if (response.ok) {
+        const newRaffle = await response.json();
+        console.log(newRaffle);
+        // Reset the key state to its initial values
+        setKey({
+          name: "",
+          key: ""
+        });
+      } else {
+        throw new Error("Something went wrong");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+
+
+  return (
+    <div className="home__wrapper">
+      <div className="home__title">Raffle App</div>
+      <div className="home">
+        <form onSubmit={handleSubmit}>
+        <h3>New Raffle</h3>
+        <label htmlFor="">Raffle Name:*</label>
+        <input type="text" name="name" value={key.name} onChange={handleChange} required />
+        <label htmlFor="">Raffle Secret Token*</label>
+        <input type="text" name="key" value={key.key} onChange={handleChange} required />
+        <div>
+          You must remeber the Raffle Token beacuse it will be asked when
+          picking a winner
+        </div>
+        <button type="submit">Create New Raffle</button>
+        </form>
+      </div>
+      <AllRaffles raffles={raffles} />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Registar.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+import "../style/Registar.css";
+
+import Create from "../components/Create";
+import Users from "../components/Users";
+import Box from "@mui/material/Box";
+import Divider from "@mui/material/Divider";
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import GroupsIcon from '@mui/icons-material/Groups';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+
+function Registar({ raffles }) {
+  const [selectedOption, setSelectedOption] = useState('registar');
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="registar">
+      <div className="title__raffle">{raffles.name}</div>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          border: "1px solid",
+          borderColor: "divider",
+          borderRadius: 1,
+          bgcolor: "background.paper",
+          color: "text.secondary",
+          "& svg": {
+            m: 1,
+          },
+          "& hr": {
+            mx: 8,
+          },
+        }}>
+       <Link to='/' style={{textDecoration:'none', color:'black'}}> <div>
+          <ConfirmationNumberIcon/>All Raffles
+        </div></Link>
+        <Divider orientation="vertical" flexItem />
+        <div className="option__1" onClick={() => handleOptionClick('registar')}>
+          <HowToRegIcon/>Registar
+        </div>
+        <Divider orientation="vertical" flexItem />
+        <div className="option__1" onClick={() => handleOptionClick('participants')}>
+          <GroupsIcon/>Participants
+        </div>
+        <Divider orientation="vertical" flexItem />
+        <div className="option__1" onClick={() => handleOptionClick('pickWinner')}>
+          <EmojiEventsIcon/>Pick Winner
+        </div>
+      </Box>
+      <div className="option">
+        {selectedOption === 'registar' && <Create />}
+        {selectedOption === 'participants' && <Users />}
+      </div>
+    </div>
+  );
+}
+
+export default Registar;
