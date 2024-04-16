24c453e6673b97dc260485a07fc0ffeb71f9d846
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2024-04-15 21:52:13 -0400

    formatted date and made put to update the specific raffle when winner is selected

--- src/components/Winner.jsx
+++ src/components/Winner.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
-
 import ConfettiExplosion from "react-confetti-explosion";
 import pic from "../components/pic/winner.jpeg";
 import '../style/Winner.css'
-
 import EmailIcon from "@mui/icons-material/Email";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import { useParams } from "react-router-dom";
@@ -14,37 +12,73 @@ function Winner({ participants }) {
   const [winner, setWinner] = useState([]);
   const [token, setToken] = useState("");
   const [clicked, setClicked] = useState(false);
-  const [ raffleById, setRaffleById ] = useState("");
+  const [raffleById, setRaffleById] = useState("");
   const { id } = useParams();
-
   const [isExploding, setIsExploding] = useState(true);
+  const [invalidToken, setInvalidToken] = useState(false);
+
+  const formatDate = (timestamp) => {
+    const options = { 
+      weekday: 'short',
+        day: 'numeric', 
+        month: 'short', 
+        year: 'numeric', 
+        hour: '2-digit', 
+        minute: '2-digit', 
+        second: '2-digit', 
+        hour12: true 
+    };
+    return new Date(timestamp).toLocaleString('en-US', options);
+};
 
   const handleChange = (e) => {
     setToken(e.target.value);
+    setInvalidToken(false); 
   };
 
-  const handleClick = () => {
-    if(raffleById.key === token ){
-        const pick = participants[Math.floor(Math.random() * participants.length)];
-        setWinner(pick);
-        setClicked(true);
+  const handleClick = async () => {
+    if (raffleById.key === token) {
+      const pick = participants[Math.floor(Math.random() * participants.length)];
+      setWinner(pick);
+      setClicked(true);
+      await updateRaffle(pick.id);
+    } else {
+      setInvalidToken(true); 
     }
   };
 
   const fetchRaffle = async () => {
     try {
-        const response = await fetch(`${API}/api/raffles/${id}`)
-        const { data } = await response.json();
-        if(response.ok){
-            setRaffleById(data)
-        }
-    }catch(err){
-        console.log(err.message);
+      const response = await fetch(`${API}/api/raffles/${id}`);
+      const { data } = await response.json();
+      if (response.ok) {
+        setRaffleById(data);
+      }
+    } catch (err) {
+      console.log(err.message);
     }
-  }
+  };
 
+  const updateRaffle = async (winnerId) => {
+    try {
+      const response = await fetch(`${API}/api/raffles/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ winner_id: winnerId }),
+      });
+      if(response.ok){
+        console.log("success")
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+  
   useEffect(() => {
     fetchRaffle();
+    updateRaffle();
   }, []);
 
   return (
@@ -56,9 +90,10 @@ function Winner({ participants }) {
             type="text"
             value={token}
             onChange={handleChange}
-            placeholder="Serect token"
+            placeholder="Enter Secret Token"
             required
           />
+          {invalidToken && <div className="invalid__token">Wrong secret token. Please try again.</div>}
           <button onClick={handleClick}>Pick a Winner</button>
           <div className="pick__footer">
             <div className="footer__title">Secret Token</div>
@@ -74,13 +109,13 @@ function Winner({ participants }) {
           <div>{isExploding && <ConfettiExplosion />}</div>
           <img src={pic} alt="we got winner" />
           <div>{winner.name}</div>
-          <div>Registered on: {winner.created_at}</div>
+          <div>Registered on: {formatDate(winner.created_at)}</div>
           <hr />
           <div># {winner.id}</div>
           <div>
             <EmailIcon />
             {winner.email}
-                  </div>
+          </div>
           <div>
             <LocalPhoneIcon />
             {winner.phone}
